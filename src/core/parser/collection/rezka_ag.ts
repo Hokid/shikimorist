d256f8efa002b9c11d41934747fab2e4852285b3
Dedonych <75681154+Dedonych@users.noreply.github.com>
2023-04-22 00:45:22 +0300

    Update rezka_ag.ts

--- src/core/parser/collection/rezka_ag.ts
+++ src/core/parser/collection/rezka_ag.ts
@@ -1,12 +1,19 @@
 import {IParser} from '../types';
 
 export class RezkaAg implements IParser {
-    checkUrl(host: string, path: string): boolean {
-        return host === 'rezka.ag' && /\/animation\/\w+\/\d+/.test(path);
-    }
+	hosts = [
+          {
+            url: 'https://rezka.ag',
+            vpn: true
+          }
+    	];
 
-    parse(document: Document): string | null {
-        const titleName = document.querySelector(".b-post__origtitle")
+    	checkUrl(host: string, path: string): boolean {
+        	return host === 'rezka.ag' && /\/animation\/\w+\/\d+/.test(path);
+    	}
+
+    	parse(document: Document): string | null {
+        	const titleName = document.querySelector(".b-post__origtitle")
 		const tmp = titleName.textContent.split(' / ')
 		return tmp[0]?tmp[0]:null
     }
